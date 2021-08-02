a49a9a63f4dba3031b78853dadaf19125c842927
William Luzitano <wluzitano@gmail.com>
2021-08-02 20:55:28 -0300

    actuator for runtime config refresh

--- hr-worker/src/main/java/com/devsuperior/hrworker/controllers/WorkerController.java
+++ hr-worker/src/main/java/com/devsuperior/hrworker/controllers/WorkerController.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,7 +15,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
+//Refresh Scope serve para usar com actuator para atualizar as configs sem derrubar o server
+@RefreshScope
 @RestController
 @RequestMapping(value = "/workers")
 public class WorkerController {
