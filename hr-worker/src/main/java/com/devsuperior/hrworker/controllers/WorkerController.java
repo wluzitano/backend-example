c1b6afdca4aa35f1f5aa0e26e78ac6a49b1388c3
William Luzitano <wluzitano@gmail.com>
2021-08-02 20:47:21 -0300

    hr-worker as a config client

--- hr-worker/src/main/java/com/devsuperior/hrworker/controllers/WorkerController.java
+++ hr-worker/src/main/java/com/devsuperior/hrworker/controllers/WorkerController.java
@@ -1,10 +1,11 @@
 package com.devsuperior.hrworker.controllers;
 
-import com.devsuperior.hrworker.services.WorkerService;
 import com.devsuperior.hrworker.entities.Worker;
+import com.devsuperior.hrworker.services.WorkerService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,77 +16,25 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMappi<?xml version="1.0" encoding="UTF-8"?>
-<project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-	xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 https://maven.apache.org/xsd/maven-4.0.0.xsd">
-	<modelVersion>4.0.0</modelVersion>
-	<parent>
-		<groupId>org.springframework.boot</groupId>
-		<artifactId>spring-boot-starter-parent</artifactId>
-		<version>2.3.4.RELEASE</version>
-		<relativePath/> <!-- lookup parent from repository -->
-	</parent>
-	<groupId>com.devsuperior</groupId>
-	<artifactId>hr-config-server</artifactId>
-	<version>0.0.1-SNAPSHOT</version>
-	<name>hr-config-server</name>
-	<description>Config Server</description>
-
-	<properties>
-		<java.version>11</java.version>
-		<spring-cloud.version>Hoxton.SR8</spring-cloud.version>
-	</properties>
-
-	<dependencies>
-		<dependency>
-			<groupId>org.springframework.cloud</groupId>
-			<artifactId>spring-cloud-config-server</artifactId>
-		</dependency>
-
-		<dependency>
-			<groupId>org.springframework.boot</groupId>
-			<artifactId>spring-boot-starter-test</artifactId>
-			<scope>test</scope>
-			<exclusions>
-				<exclusion>
-					<groupId>org.junit.vintage</groupId>
-					<artifactId>junit-vintage-engine</artifactId>
-				</exclusion>
-			</exclusions>
-		</dependency>
-	</dependencies>
-
-	<dependencyManagement>
-		<dependencies>
-			<dependency>
-				<groupId>org.springframework.cloud</groupId>
-				<artifactId>spring-cloud-dependencies</artifactId>
-				<version>${spring-cloud.version}</version>
-				<type>pom</type>
-				<scope>import</scope>
-			</dependency>
-		</dependencies>
-	</dependencyManagement>
-
-	<build>
-		<plugins>
-			<plugin>
-				<groupId>org.springframework.boot</groupId>
-				<artifactId>spring-boot-maven-plugin</artifactId>
-			</plugin>
-		</plugins>
-	</build>
-
-</project>ng(value = "/workers")
+@RequestMapping(value = "/workers")
 public class WorkerController {
 
     private static Logger logger = LoggerFactory.getLogger(WorkerController.class);
+
+    @Value("${test.config}")
+	private String testConfig;
 
     @Autowired
     private Environment env;
 
     @Autowired
     private WorkerService workerService;
+
+	@GetMapping(value="/configs")
+	public ResponseEntity<Void> getConfigs() {
+		logger.info("Config = ".concat(testConfig));
+		return ResponseEntity.noContent().build();
+	}
 
     @GetMapping
     public ResponseEntity<List<Worker>> findAll() {
