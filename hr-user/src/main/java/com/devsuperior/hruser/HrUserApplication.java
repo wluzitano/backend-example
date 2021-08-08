59227f71246b35ca6803325d94e42fec8940ee01
William Luzitano <wluzitano@gmail.com>
2021-08-07 21:02:33 -0300

    Database seeding

--- hr-user/src/main/java/com/devsuperior/hruser/HrUserApplication.java
+++ hr-user/src/main/java/com/devsuperior/hruser/HrUserApplication.java
@@ -1,15 +1,26 @@
 package com.devsuperior.hruser;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableEurekaClient
 @SpringBootApplication
-public class HrUserApplication {
+public class HrUserApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HrUserApplication.class, args);
-	}
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
 
+    public static void main(String[] args) {
+        SpringApplication.run(HrUserApplication.class, args);
+    }
+
+    //So usado para gerar um passsword encriptado pra se quiser salvar no banco
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("BCRYPT = " + passwordEncoder.encode("123456"));
+    }
 }
