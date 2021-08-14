fc6ffc111ff3da3c7e53a049275b2ce329f515c4
William Luzitano <wluzitano@gmail.com>
2021-08-14 20:31:12 -0300

    Deletating access to auth users

--- hr-oauth/src/main/java/com/devsuperior/hroauth/config/SecurityConfig.java
+++ hr-oauth/src/main/java/com/devsuperior/hroauth/config/SecurityConfig.java
@@ -1,11 +1,18 @@
 package com.devsuperior.hroauth.config;
 
+import com.netflix.ribbon.proxy.annotation.Http;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
@@ -17,6 +24,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private UserDetailsService userDetailsService;
+
+    private static Logger logger = LoggerFactory.getLogger(SecurityConfig.class);
 
     @Override
     @Autowired
@@ -30,4 +39,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected AuthenticationManager authenticationManager() throws Exception {
         return super.authenticationManager();
     }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers(HttpMethod.GET, "/users/search/**").permitAll();
+    }
+
 }
